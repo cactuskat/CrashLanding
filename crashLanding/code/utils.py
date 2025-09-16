9c92a977dc5bebe1cd52be72f975be79b95318d3
cactuskat <seriousbusiness401@gmail.com>
2025-09-15 20:15:59 -0400

    Changed load assets to gameroot or meipass
    changed how the path for each asset is loaded in depending on if loading within folder or executable(meipass)

--- crashLanding/code/utils.py
+++ crashLanding/code/utils.py
@@ -4,6 +4,18 @@ from os.path import abspath,dirname
 BASE_DIR = dirname(__file__)
 GAME_ROOT = abspath(join(BASE_DIR, ".."))
 
+def get_full_path(*path):
+    """
+    Get full path for assets in both dev mode & PyInstaller executable
+    """
+    if hasattr(sys, '_MEIPASS'):
+        #executable: assets relative to sys._MEIPASS
+        return join(sys._MEIPASS, *path)
+    else:
+         #dev mode: assests relavtive to GAME_ROOT
+         return join(GAME_ROOT, *path)
+
+
 def safe_load(func):
     def wrap(*args,**kwargs):
         try:
@@ -14,7 +26,7 @@ def safe_load(func):
 
 @safe_load
 def load_asset(*path) -> str:
-    return join(GAME_ROOT, *path)
+    return get_full_path(*path)
 
 @safe_load
 def load_audio(*path) -> pygame.mixer.Sound:
